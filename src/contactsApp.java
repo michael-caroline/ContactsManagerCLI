8db15994b6e1eec4d05f035e00774139f3e31231
Caroline E <caroline.esquivel01@gmail.com>
2020-10-19 10:56:43 -0500

    changed to objects

--- src/contactsApp.java
+++ src/contactsApp.java
@@ -1,30 +1,26 @@
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class contactsApp {
     public static void main(String[] args) throws IOException {
         Path contactsPath = Paths.get("contacts.txt");
 
-        List<String> linesList = Files.readAllLines(contactsPath);
-        List<Contact> contactsList = new ArrayList<Contact>();
-        for (String line : linesList) {
-            String name = line.substring(0, line.indexOf("|")).trim();
-            String number = line.substring(line.indexOf("|")).trim();
-            Contact thisContact = new Contact(name, number);
-            contactsList.add(thisContact);
+        File file = new File("contactsPath");
+        ArrayList<Contact> contactsList = new ArrayList<Contact>();
+        if (file.length() > 0) {
+            contactsList = readContacts(contactsPath, contactsList);
         }
-
 
         Input userInput = new Input();
         boolean wantstoUseApp = true;
-        while(wantstoUseApp) {
+        while (wantstoUseApp) {
             System.out.println("1. View contacts.\n" +
                     "2. Add a new contact.\n" +
                     "3. Search a contact by name.\n" +
@@ -32,14 +28,14 @@ public class contactsApp {
                     "5. Exit.\n" +
                     "Enter an option (1, 2, 3, 4 or 5): ");
             int userChoice = userInput.getInt(1, 5);
-            if(userChoice == 1) {
-                displayAll(contactsPath);
+            if (userChoice == 1) {
+                displayAll(contactsList);
             } else if (userChoice == 2) {
                 addContact(contactsList);
             } else if (userChoice == 3) {
-                contactSearch(contactsPath);
+                contactSearch(contactsList);
             } else if (userChoice == 4) {
-                deleteContact(contactsPath);
+                deleteContact(contactsList);
             } else if (userChoice == 5) {
                 System.out.println("Goodbye!");
                 break;
@@ -47,8 +43,7 @@ public class contactsApp {
         }
     }
 
-    public static void displayAll(Path contactsPath) throws IOException {
-        List<String> contactsList = Files.readAllLines(contactsPath);
+    public static void displayAll(ArrayList <Contact> contactsList) throws IOException {
         System.out.println("Name  |  Phone Number\n------------------------");
         for (int i = 0; i < contactsList.size(); i++) {
             System.out.println(contactsList.get(i));
@@ -57,7 +52,7 @@ public class contactsApp {
         System.out.println();
     }
 
-    public static void addContact(List contactsList) throws IOException {
+    public static void addContact(ArrayList <Contact> contactsList) throws IOException {
         Input userInput = new Input();
         System.out.println("Please, enter the new contact's name: ");
         String name = userInput.getString();
@@ -67,8 +62,7 @@ public class contactsApp {
         contactsList.add(newContact);
     }
 
-    public static boolean contactSearch(Path contactsPath) throws IOException {
-        List<String> contactsList = Files.readAllLines(contactsPath);
+    public static boolean contactSearch(ArrayList <Contact> contactsList) throws IOException {
         System.out.println("Search a name");
         Input userInput = new Input();
         String nameToSearchFor = userInput.getString();
@@ -81,21 +75,32 @@ public class contactsApp {
         }
     }
 
-    public static void deleteContact(Path contactPath) throws IOException {
-        List<String> fileContents = Files.readAllLines(contactPath);
-        List<String> modifiedList = new ArrayList<>();
+    public static ArrayList<Contact> deleteContact(ArrayList <Contact> contactsList) throws IOException {
+        ArrayList<Contact> modifiedList = new ArrayList<>();
         System.out.print("Enter which contact you'd like to delete: ");
         Input userInput = new Input();
         String unwantedContact = userInput.getString();
-        if (!fileContents.contains(unwantedContact)) {
+        if (!contactsList.contains(unwantedContact)) {
             System.out.println("Contact not found!");
         }
-        for (String item: fileContents) {
-            if(!item.equals(unwantedContact)) {
-                modifiedList.add(item);
+        for (int i = 0; i < contactsList.size(); i++) {
+            if (!contactsList.get(i).getName().equals(unwantedContact)) {
+                modifiedList.add(contactsList.get(i));
             }
         }
-        Files.write(contactPath, modifiedList);
+       return modifiedList;
+    }
+
+    public static ArrayList<Contact> readContacts(Path contactsPath, ArrayList <Contact> contactsList) throws IOException {
+        List<String> linesList = Files.readAllLines(contactsPath);
+
+        for (String line : linesList) {
+            String name = line.substring(0, line.indexOf("|")).trim();
+            String number = line.substring(line.indexOf("|")).trim();
+            Contact thisContact = new Contact(name, number);
+            contactsList.add(thisContact);
+        }
+        return contactsList;
     }
 
 }
